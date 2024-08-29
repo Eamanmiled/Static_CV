83668163ade1a8b18dbf9f5403ee097b1739aaaa
Eamann Miled <130555562+Eamanmiled@users.noreply.github.com>
2024-08-29 12:14:48 +0100

    pages now have content and can be navigated

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//side bar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
+//side bar
