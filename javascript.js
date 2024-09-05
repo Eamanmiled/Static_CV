1d1f45cf97c896b07b43e3952e3cc57668e01d24
Eamann Miled <130555562+Eamanmiled@users.noreply.github.com>
2024-09-05 02:48:16 +0100

    redone as last was ass

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+  document.getElementById('content').classList.add('visible');
+};
+
 //side bar
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
